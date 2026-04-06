6cc6c12ab1791cda26e8df169dffa809aa48034e
Richa Mandhan <richamandhan65@gmail.com>
2026-04-06 16:49:55 +0530

    Created Home Page , Created /,get,post routes. Created APIs that will create the Custom object record and get apis that will get the dropdown data and the custom object records,  created views using pub templates for Home page and the custom object record creation form and integrated custom object records to the Home page, Added record creation and back to home page links.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,19 +9,70 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    // Get Custom Object Data
+    const memberships = 'https://api.hubspot.com/crm/v3/objects/2-227693084?properties=name,membership_type,start_date,end_date';
+    const headers = {
+        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(memberships, { headers });
+        const data = resp.data.results;
+        res.render('home', { title: 'Home Page', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    // Get Custom Object Property (Memebership Type) Data through the API to populate the dropdown in the form
+    const membership_types = 'https://api.hubspot.com/crm/v3/properties/2-227693084/membership_type';
+    const headers = {
+        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(membership_types, { headers });
+        const membershipOptions = resp?.data?.options;
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum.', membershipOptions });    
+    } catch (error) {
+        console.error(error);
+    } 
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    // Create Custom Object Record through the API using the form data
+    console.log(req.body);
+    const createMembership = {
+        properties: {
+            "name": req.body.name,
+            "membership_type": req.body.membership_type,
+            "start_date": req.body.start_date,
+            "end_date": req.body.end_date
+        }
+    }
+
+    const membership = 'https://api.hubspot.com/crm/v3/objects/2-227693084';
+    const headers = {
+        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.post(membership, createMembership, { headers } );
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -68,4 +120,4 @@ app.post('/update', async (req, res) => {
 
 
 // * Localhost
-app.listen(6000, () => console.log('Listening on http://localhost:6000'));
+app.listen(3000, () => console.log('Listening on http://localhost:3000'));
